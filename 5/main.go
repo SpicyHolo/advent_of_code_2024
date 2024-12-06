d282f2ef32d8ac2924ac2d60c1bb88a43264b8c4
Konrad Makowski <41269364+SpicyHolo@users.noreply.github.com>
2024-12-06 21:12:05 +0100

    remove execution timer

--- 5/main.go
+++ 5/main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func readInput(filename string) (map[int][]int, [][]int, error) {
@@ -14,6 +13,7 @@ func readInput(filename string) (map[int][]int, [][]int, error) {
 	if err != nil {
 		return nil, nil, fmt.Errorf("Cannot read file: %w", err)
 	}
+	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
@@ -75,6 +75,17 @@ func readInput(filename string) (map[int][]int, [][]int, error) {
 	Searching orderMap set, worst case O(m), average O(1)
 
 	Average: O(n+m), worst-case either O(n*m) or O(n^2) depending whether there are more rules or nums in input
+
+	Algorithm:
+	pre: Create a map for all map[Y] X
+
+	1. Go through each number in an update
+	2. Add the number to Appeared set
+	3. Check if it's in CannotAppear set, if it is result is False
+	4. Check in rules map, if so add every number that had to appear before it, and didn't to CannotAppear set
+		(Those numbers cannot appear, because according to rules they had to appear before current number, but didn't)
+	5. If gone through the whole list, return True
+
 */
 func validateInput(input []int, orderMap map[int][]int) bool {
 	cannotAppear := make(map[int]bool)
@@ -103,45 +114,90 @@ func validateInput(input []int, orderMap map[int][]int) bool {
 	return true
 }
 
+// Part II solution
+// O(n(n+m)), assuming less rules than 'updates' -> O(n^2)
+/*
+	Brute Force, (I'm tired boss...)
+	Use the same algorithm as in Part I, but
+	but now under each key in cannotAppear we save and index of the number that inforced that rule.
+	If we reach such number, we swap the rule breaking number, with rule enforcing number, and
+	call the function on the new array.
+	Do this, until the entire array is valid, and return the result array.
+
+	Probably can try a linear solution, where you need to decide how to update catnnotAppear, appeared maps, after the swap.
+	Redoing the rule checking for swapped number (since after the swap, more numbers come after it), could do the trick. (Will re-visit TODO)
+*/
+func partTwo(input []int, orderMap map[int][]int) []int {
+	cannotAppear := make(map[int]int)
+	appeared := make(map[int]bool)
+
+	for i, num := range input {
+		appeared[num] = true
+
+		// Check if this number can appear
+		_, ok := cannotAppear[num]
+		if ok {
+			// Swap and call PartTwo with new array
+			j := cannotAppear[num]
+			input[i], input[j] = input[j], input[i]
+			return partTwo(input, orderMap)
+		}
+
+		// Check if any numbers need to appear before it,
+		pairs, ok := orderMap[num]
+		if ok {
+			// If pair needs to appear before num, check if it did, if not add it to cannotAppear
+			for _, pair := range pairs {
+				if !appeared[pair] {
+					_, ok := cannotAppear[num]
+					if !ok {
+						cannotAppear[pair] = i
+					}
+				}
+			}
+
+		}
+	}
+	return input
+}
+
 func main() {
-	start := time.Now()
+	// Read inputs, create map of rules
 	orderMap, input, err := readInput("input.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// for k, v := range orderMap {
-	// 	fmt.Printf("%d | %v\n", k, v)
-	// }
-	// fmt.Println()
+	fmt.Println("Rules:")
+	for v, k := range orderMap {
+		fmt.Printf("%d | %v\n", k, v)
+	}
 
+	fmt.Println("Num of. Values: ", len(input))
+	fmt.Println()
+
+	// Incorrect inputs for the second part
+	var incorrect_inputs [][]int
+
+	// Part I solution, O(n + m), assuming less rules than 'updates' -> O(n)
 	sum := 0
 	for _, line := range input {
 		if validateInput(line, orderMap) {
 			middle_value := line[len(line)/2]
 			sum += middle_value
 		} else {
-			//fmt.Println(line)
+			incorrect_inputs = append(incorrect_inputs, line)
 		}
 	}
 
-	duration := time.Since(start)
-	fmt.Printf("Total execution time: %v\n", duration)
-	fmt.Println("sum: ", sum)
+	fmt.Println("Part I sum: ", sum)
+
+	sum = 0
+	for _, line := range incorrect_inputs {
+		fixed_line := partTwo(line, orderMap)
+		middle_value := fixed_line[len(fixed_line)/2]
+		sum += middle_value
+	}
+	fmt.Println("Part II sum:", sum)
 }
-
-/* Part I
-Easy linear solution: T: O(n+m), M: O(n+m), 1.5ms on go with reading file
-*/
-
-/* Part II
-Trying to reuse my above solution:
-- Previously i had a set of numbers, that broke the rules if they appeared next,
-	now if we get to such a number, we need to know why it broke the rules, so i also need a map.
-- So If we get to a number that breaks a rule we need to move it before its pair, how does this affect the rules concerning that pair
-
-- Probably if we move a number to a previous spot, we need to re-evaluate its rule.
-	Not sure if its gonna be straigtforward how we update CannotAppear, after such re-order, or do we start algorithm over with clean state?
-	(Not gonna worry about make it linear for now.)
-*/
